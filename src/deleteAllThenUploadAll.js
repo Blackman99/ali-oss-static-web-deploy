170065b506b619ab7ceb3e8598a43d06afffff3c
Zhao DongSheng <1197160272@qq.com>
2021-07-08 16:08:56 +0800

    uplaod files single uploading tip message

--- src/deleteAllThenUploadAll.js
+++ src/deleteAllThenUploadAll.js
@@ -1,6 +1,7 @@
 const getAllBucketFiles = require('./getAllBucketFiles')
 const getUploadFiles = require('./getUploadFiles')
 const path = require('path')
+const chalk = require('chalk')
 const { logInfo, logDanger } = require('./logUtils')
 
 // delete all bucket exits files, then upldoate all static files
@@ -20,9 +21,11 @@ const deleteAllThenUploadAll = async (aliOssClient, staticWebAppPath) => {
   logInfo(`Uploading new files...`)
   for(let i = 0; i < newFiles.length; i++) {
     const [localPath, remotePath] = newFiles[i]
+    process.stdout.write("\r\x1b[K")
+    process.stdout.write(`${chalk.yellow('Uploading local file: ')}${localPath} ${chalk.yellow('To Ali OSS: ')}${remotePath}\x1b[K`)
     await aliOssClient.put(remotePath, path.normalize(localPath))
   }
-  logInfo(`Upload new files done`)
+  logInfo(`\nUpload new files done`)
 }
 
 module.exports = deleteAllThenUploadAll
