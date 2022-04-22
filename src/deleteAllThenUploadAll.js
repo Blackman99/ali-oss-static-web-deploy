6937709c90eac7df612f5bbc530f616a4e54c91e
Dongsheng <1197160272@qq.com>
2022-04-22 11:35:22 +0800

    feat: add compare new uploaded files and old files, only delete old different files after upload done

--- src/deleteAllThenUploadAll.js
+++ src/deleteAllThenUploadAll.js
@@ -4,28 +4,36 @@ const path = require('path')
 const chalk = require('chalk')
 const { logInfo, logDanger } = require('./logUtils')
 
-// delete all bucket exits files, then upldoate all static files
+// delete all bucket exits files, then upload all static files
 const deleteAllThenUploadAll = async (aliOssClient, staticWebAppPath) => {
 
-  logInfo(`Indexing bucket files...`)
+  logInfo(`Indexing existing files...`)
   const oldFiles = await getAllBucketFiles(aliOssClient)
-  logInfo(`Bucket files indexed`)
+  logInfo(`Existing files indexed`)
 
-  if (oldFiles && oldFiles.length > 0) {
-    logDanger(`Deleting bucket files...`)
-    await aliOssClient.deleteMulti(oldFiles.map(({ name }) => name), { quiet: true })
-    logDanger(`Bucket files deleted`)
-  }
-  const newFiles = await getUploadFiles(staticWebAppPath)
+  const filesToUpload = await getUploadFiles(staticWebAppPath)
 
   logInfo(`Uploading new files...`)
-  for(let i = 0; i < newFiles.length; i++) {
-    const [localPath, remotePath] = newFiles[i]
+  for(let i = 0; i < filesToUpload.length; i++) {
+    const [localPath, remotePath] = filesToUpload[i]
     process.stdout.write("\r\x1b[K")
     process.stdout.write(`${chalk.yellow('Uploading local file: ')}${localPath} ${chalk.yellow('To Ali OSS: ')}${remotePath}\x1b[K`)
     await aliOssClient.put(remotePath, path.normalize(localPath))
   }
   logInfo(`\nNew files uploaded`)
+  
+  
+  const newFiles = await getAllBucketFiles(aliOssClient)
+  logInfo(`Comparing existing files and new uploaded files...`)
+  const filesToDelete = oldFiles.filter(oldFile => !newFiles.some(newFile => newFile.name === oldFile.name))
+  console.log('filesToDelete: ', filesToDelete)
+  if (filesToDelete && filesToDelete.length > 0) {
+    logDanger(`Deleting old files...`)
+    await aliOssClient.deleteMulti(filesToDelete.map(({ name }) => name), { quiet: true })
+    logDanger(`Old files deleted`)
+  }
+
+  logInfo(`All done.`)
 }
 
 module.exports = deleteAllThenUploadAll
